c377fac038d8050a06d90317592ccf375b4322b6
tymcio01 <149621249+tymcio01@users.noreply.github.com>
2023-11-02 09:42:31 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,11 +2,8 @@
 function register(email, password) {
     firebase.auth().createUserWithEmailAndPassword(email, password)
         .then((userCredential) => {
-            // Użytkownik zarejestrowany, przekierowanie do dashboard.html
-            setTimeout(function() {
-    window.location.href = 'dashboard.html';
-}, 1000); // Przekierowanie po 1 sekundzie
-
+            // Użytkownik zarejestrowany, bezpośrednie przekierowanie do dashboard.html
+            window.location.href = 'dashboard.html';
         })
         .catch((error) => {
             var errorCode = error.code;
@@ -19,10 +16,8 @@ function register(email, password) {
 function login(email, password) {
     firebase.auth().signInWithEmailAndPassword(email, password)
         .then((userCredential) => {
-            // Użytkownik zalogowany, przekierowanie do dashboard.html
-                 setTimeout(function() {
-    window.location.href = 'dashboard.html';
-}, 1000); // Przekierowanie po 1 sekundzie
+            // Użytkownik zalogowany, bezpośrednie przekierowanie do dashboard.html
+            window.location.href = 'dashboard.html';
         })
         .catch((error) => {
             var errorCode = error.code;
